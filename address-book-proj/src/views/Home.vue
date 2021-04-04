ad41c41596a74e2bc40d372b4336875267df5f9b
Krutika Patil <krutiks89@gmail.com>
2021-04-03 23:22:12 -0400

    fix #7: Added functionality to 'Add New' button

--- address-book-proj/src/views/Home.vue
+++ address-book-proj/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container-fluid">
     <div class="row">
       <div class="col-md-12 col-sm-12 col-xs-12 mt-5">
         <table class="table">
@@ -15,26 +15,51 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="item in items" :key=item.id :class="{'row-selected': selectedItems.find(i => i.id === item.id)}">
+            <tr v-for="item in items" :key=item.id class="table-row" :class="{'row-selected': selectedItems.find(i => i.id === item.id), 'new-row': item.id === ''}">
               <td>
-      <input type="checkbox" class="checkbox" @change="rowSelected(item)">
+      <input type="checkbox" class="checkbox" @change="rowSelected(item)" :disabled="item.id === ''">
               </td>
               <td>{{item.id}}</td>
-              <td>{{item.name}}</td>
-              <td>{{item.location}}</td>
-              <td>{{item.building}}</td>
-              <td>{{item.office}}</td>
-              <td @dblclick="cellClicked">{{item.cell}}</td>
+              <td class="name">
+                <span v-if="item.id === ''">
+                  <input type="text" v-model="item.name">
+                </span>
+                <span v-else>{{item.name}}</span>
+              </td>
+              <td class="location">
+                <span v-if="item.id === ''">
+                  <input type="text" v-model="item.location">
+                </span>
+                <span v-else>{{item.location}}</span>
+              </td>
+              <td class="building">
+                <span v-if="item.id === ''">
+                  <input type="text" v-model="item.building">
+                </span>
+                <span v-else>{{item.building}}</span>
+              </td>
+              <td class="office">
+                <span v-if="item.id === ''">
+                  <input type="text" v-model="item.office">
+                </span>
+                <span v-else>{{item.office}}</span>
+              </td>
+              <td class="cell" @dblclick="cellClicked">
+                <span v-if="item.id === ''">
+                  <input type="text" v-model="item.cell">
+                </span>
+                <span v-else>{{item.cell}}</span>
+              </td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
     <div class="row">
-      <div class="col-md-3 col-sm-3 col-xs-3">
-        <button class="btn btn-success">Add New</button>
+      <div class="col-md-3 col-sm-3 col-xs-3" style="text-align:right">
+        <button class="btn btn-success" @click="addNewItem">Add New</button>
       </div>
-      <div class="col-md-3 col-sm-3 col-xs-3">
+      <div class="col-md-3 col-sm-3 col-xs-3" style="text-align:left">
         <button class="btn btn-warning">Update</button>
       </div>
       <div class="col-md-3 col-sm-3 col-xs-3 offset-md-3 offset-sm-3 offset-xs-3">
@@ -96,6 +121,18 @@ export default {
     },
     deleteRecords() {
       this.items = this.items.filter(item => !this.selectedItems.find(selectedItem => selectedItem.id === item.id));
+    },
+    addNewItem() {
+      const newItem = {
+        id: '',
+        name: '',
+        location: '',
+        building: '',
+        office: '',
+        cell: ''
+      }
+      this.items.push(newItem);
+     
     }
   }
 }
@@ -108,5 +145,17 @@ export default {
 
 .checkbox {
   cursor: pointer;
+}
+
+input {
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  max-width: 80%;
+}
+
+input:focus {
+    outline: none !important;
+    border:1px solid red;
+    box-shadow: 0 0 10px #719ECE;
 }
 </style>
